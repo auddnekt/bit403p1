e4edf7658585e6f0eadd044afd9d219d7dccc273
unknown <auddnekt@naver.com>
2019-12-22 16:55:46 +0900

    헤더구분

--- js/Pub&BAR.js
+++ js/Pub&BAR.js
@@ -19,38 +19,17 @@ $(document).ready(function(){
        });
 
 
-
     $('#art1 .no1').on('click', function(){
-        $('.btn1').show();
-        $('.btn2').hide();
-        $('.btn3').hide();
-        $('#mainSlider>#sel1').show();
-        $('#mainSlider>#sel2').hide();
-        $('#mainSlider>#sel3').hide();
         $('#poster1').show();
         $('#poster2').hide();
         $('#poster3').hide();
     });
     $('#art1 .no2').on('click', function(){
-        $('.btn2').show();
-        $('.btn1').hide();
-        $('.btn3').hide();
-        $('#mainSlider>#sel2').show();
-        $('#mainSlider>#sel1').hide();
-        $('#mainSlider>#sel3').hide();
         $('#poster2').show();
         $('#poster1').hide();
         $('#poster3').hide();
-
-
     });
     $('#art1 .no3').on('click', function(){
-        $('.btn3').show();
-        $('.btn1').hide();
-        $('.btn2').hide();
-        $('#mainSlider>#sel3').show();
-        $('#mainSlider>#sel2').hide();
-        $('#mainSlider>#sel1').hide();
         $('#poster3').show();
         $('#poster1').hide();
         $('#poster2').hide();
